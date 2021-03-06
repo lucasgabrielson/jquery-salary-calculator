fc5d62f5221f7c5e55f56358c243f542d936b9c6
Lucas Gabrielson <>
2021-03-05 20:48:26 -0600

    completed showEmployees and calculateMonthly

--- README.md
+++ README.md
@@ -7,3 +7,16 @@
 Your project description goes here. What problem did you solve? How did you solve it?
 
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+
+Create Input Fields and Submit button
+
+On Click Take information from the inputs and push them into an array
+
+Append the Information to the DOM (including deleted button)
+
+Keep track of Monthly Salaries
+
+Append Total payroll to DOM
+
+
+
--- scripts/client.js
+++ scripts/client.js
@@ -1,1 +1,53 @@
-console.log( 'js ');
+$( document ).ready( readyNow );
+
+let employees = [];
+
+function readyNow() {
+    // click listeners
+    $( '#submit' ).on( 'click', addEmployee );
+} // end readyNow
+
+function addEmployee() {
+    const employee = {
+        firstName: $( '#firstName' ).val(),
+        lastName: $( '#lastName' ).val(),
+        id: $( '#idNumber' ).val(),
+        title: $( '#jobTitle' ).val(),
+        salary: $( '#salary' ).val()
+    } // end employee
+    
+    employees.push(employee);
+    showEmployees();
+    
+
+} // end addEmployee
+
+function showEmployees() {
+    let el = $( '#employeeTableBody' );
+    el.empty();
+    for( let i = 0; i < employees.length; i++) {
+        el.append(
+            `
+            <tr>
+                    <td>${employees[i].firstName}</td>
+                    <td>${employees[i].lastName}</td>
+                    <td>${employees[i].id}</td>
+                    <td>${employees[i].title}</td>
+                    <td>$${employees[i].salary}</td>
+                    <td><button>Delete</button></td>
+                </tr>
+            `
+        )
+    } // end for
+    calculateMonthly();
+}
+
+function calculateMonthly() {
+    let monthlySalary = 0;
+    let el = $( '#monthlySalary ');
+    el.empty();
+    for( let i = 0; i < employees.length; i++) {
+        monthlySalary += Number(employees[i].salary);
+    } // end for
+    el.append(`${monthlySalary / 12}`)
+} // end calculateMonthly
